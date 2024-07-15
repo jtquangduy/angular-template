f2bf05a0d1eed00ad2206914d71bcba16ab6c1ff
jtquangduy <justinquangduy@gmail.com>
2024-07-15 16:04:50 +0700

    Binding, Event Binding, Managed State & Changing Data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  selectedUsr = DUMMY_USERS[randomIndex];
 
+  get imagePath() {
+    return `/assets/users/${this.selectedUsr.avatar}`;
+  }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUsr = DUMMY_USERS[randomIndex];
+  }
 }
