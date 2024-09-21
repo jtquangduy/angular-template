c5e4ed10f9b23d377bdf3ff97217d97c2f285a84
Quang Duy <justinquangduy@gmail.com>
2024-09-21 18:34:51 +0700

    Using the output() Function

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, EventEmitter, Input, input, Output, signal } from '@angular/core';
+import { Component, computed, EventEmitter, Input, input, output, Output, signal } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -15,12 +15,13 @@ export class UserComponent {
   @Input({required:true}) avatar!: string;
   @Input({required:true}) name!: string;
 
-  @Output() select = new EventEmitter();
+  //@Output() select = new EventEmitter();
+  select = output<string>();
 
   get imagePath() {
     return `/assets/users/${this.avatar}`;
   }
-  
+
   onSelectUser() {
     this.select.emit(this.id);
   }
