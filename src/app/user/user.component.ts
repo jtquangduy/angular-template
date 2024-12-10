528a2290e866fdb4c9da60fd2f6ae92101e4b4af
jtquangduy <justinquangduy@gmail.com>
2024-12-10 10:19:48 +0700

    Storing Data Models in Separate Files

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,19 +2,9 @@ import { Component, computed, EventEmitter, Input, input, Output, signal } from 
 
 import { DUMMY_USERS } from '../dummy-users';
 
+import { type User } from './user.model'
+ 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-
-// type User = {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// };
-
-interface User{
-  id: string;
-  avatar: string;
-  name: string;
-};
 
 @Component({
   selector: 'app-user',
