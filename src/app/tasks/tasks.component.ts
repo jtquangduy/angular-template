ac5125724019640f47070d50e6ed3b098573478f
jtquangduy <justinquangduy@gmail.com>
2024-12-10 18:02:52 +0700

    Modules

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,9 @@
 import { TasksService } from './tasks.service';
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from './task/task.component';
-import { NewTaskComponent } from './new-task/new-task.component';
 import { type NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
-  standalone: true,
-  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
